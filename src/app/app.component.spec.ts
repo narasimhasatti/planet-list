6950c7db18384f50804679f85a471fca716259f1
Narasimha satti <umanarasimha.satti1@t-mobile.com>
2020-06-06 19:16:17 -0700

    changed the app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'planet-app'`, () => {
+  it(`should have as title 'planet-list'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('planet-app');
+    expect(app.title).toEqual('planet-list');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('planet-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('planet-list app is running!');
   });
 });
